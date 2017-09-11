b397c2979f3d2d2594aa7adc39d4778fa8bb2b9d
Logan McAnsh <logan@mcan.sh>
2017-09-11 11:13:31 -0400

    switch eslintrc to the js flavor
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+    node: true,
+    jquery: true
+  },
+  rules: {
+    'no-unused-vars': 1,
+    'no-console': 0,
+  },
+};
