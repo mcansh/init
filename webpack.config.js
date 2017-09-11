cff629bdd4ef612a4ca1d63b6e26e47a2ca8c07b
Logan McAnsh <logan@mcan.sh>
2017-09-11 12:00:54 -0400

    update webpack config to use env

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ const nodeEnv = process.env.NODE_ENV || 'production';
 module.exports = {
   devtool: 'source-map',
   entry: {
-    filename: './source/assets/js/main.js'
+    filename: './source/assets/js/main.js',
   },
   output: {
-    filename: './source/assets/js/bundle.js'
+    filename: './source/assets/js/bundle.js',
   },
   module: {
     loaders: [
@@ -17,21 +17,21 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015-native-modules']
-        }
-      }
-    ]
+          presets: ['env'],
+        },
+      },
+    ],
   },
   plugins: [
     // uglify js
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       output: { comments: false },
-      sourceMap: true
+      sourceMap: true,
     }),
     // env plugin
     new webpack.DefinePlugin({
-      'proccess.env': { NODE_ENV: JSON.stringify(nodeEnv) }
-    })
-  ]
+      'proccess.env': { NODE_ENV: JSON.stringify(nodeEnv) },
+    }),
+  ],
 };
